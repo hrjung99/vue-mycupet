80aff62e0aedc245235753a1ffa6d17fb9b26232
jeongmin <jeongmin0521@naver.com>
2024-05-27 17:24:20 +0900

    쇼핑몰 메인페이지, 부트 통신 위해vue.config.js 수정,

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import JoinPage from '@/components/user/JoinForm.vue';
 import FindPet from '@/components/findpet/FindPetMain.vue';
 import MyCupetPage from '@/components/MyCupetPage/MypageMain.vue';
 import MyCupetPetpage from '@/components/MyCupetPage/MyCupetPetpage.vue';
+import ShopMain from '@/components/shop/ShopMain.vue';
 
 const routes = [
     { path: '/', component: MainPage }, // 기본 경로('/')일 때 MainPage를 보여줍니다.
@@ -13,6 +14,7 @@ const routes = [
     { path: '/FindPet', component: FindPet },
     { path: '/MyCupetPage', component: MyCupetPage },
     { path: '/MyCupetPetpage', component: MyCupetPetpage },
+    { path: '/ShopMain', component: ShopMain },
 ];
 
 const router = createRouter({
