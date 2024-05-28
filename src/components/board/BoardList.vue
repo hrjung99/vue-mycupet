e5e2b425aa7650bfed429f5e8a0253a2f2277e50
paramy <rimiajung@gmail.com>
2024-05-28 11:40:43 +0900

    hyerim _board_list_ing

---
+++ src/components/board/BoardList.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <table class="board_list">
+        <tr>
+            <td></td>
+            <th>게시물 번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>작성일</th>
+            <th>조회수</th>
+        </tr>
+        <!--
+        <c:forEach var="board" items="${pageResponseVO.list}">
+        -->
+        <tr>
+            <td></td>
+            <td style="cursor:pointer;">1</td>
+            <td>2</td>
+            <td>3</td>  
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+        </tr>
+        <!--
+        </c:forEach>
+        -->
+    </table>
+        
+    </div>
+</template>
+<script>    
+export default {
+    
+}
+</script>
+<style>
+th {
+    text-align: center;
+}
+
+td {
+    text-align: left;
+}
+
+td:nth-child(2n), th:nth-child(2n) {
+    background-color: #9cd866;
+}
+
+td:nth-child(2n-1), th:nth-child(2n-1) {
+    background-color: white;
+}
+
+/* 모든 테두리를 없앰 */
+th, td {
+    border: none;
+}
+
+/* tr 요소의 아래쪽에만 테두리 추가 */
+tr {
+    border-bottom: 1px solid black;
+}
+
+/* 마지막 tr 요소의 아래쪽 테두리 제거 */
+tr:last-child {
+    border-bottom: none;
+}
+</style>
