7383d450bf76c2d8d8331f1eb5e638c66c1e8202
jeongmin <jeongmin0521@naver.com>
2024-05-31 10:29:33 +0900

    장바구니 페이지

--- src/components/shop/ShopCart.vue
+++ src/components/shop/ShopCart.vue
@@ -3,21 +3,25 @@
   <div>
     <div class="content-container">
       <CommonSideBar />
-        <div class="cart">
-          <div class="container">
-            <ul>
-              <li v-for="(item, idx) in state.items" :key="idx">
-                <img :src="item.cupet_prodimgpath" />
-                <span class="name">{{ item.cupet_prodname }}</span>
-                <span class="price">{{ lib.getNumberFormatted(item.cupet_prodprice - item.cupet_prodprice * item.cupet_proddiscountper / 100) }}원</span>
-                <i class="fa fa-trash" @click="remove(item.id)"></i>
-              </li>
-            </ul>
-            <router-link to="/order" class="btn btn-primary">구입하기</router-link>
-          </div>
+      <div class="cart">
+        <div class="container">
+          <ul>
+            <li v-for="(item, idx) in state.items" :key="idx">
+              <img :src="item.cupet_prodimgpath" />
+              <span class="name">{{ item.cupet_prodname }}</span>
+              <span class="price">{{ lib.getNumberFormatted(item.cupet_prodprice - item.cupet_prodprice * item.cupet_proddiscountper / 100) }}원</span>
+              <i class="fa fa-trash" @click="remove(item.id)"></i>
+            </li>
+          </ul>
+          <router-link to="/order">
+            <button type="button" class="buybtn">
+              구입하기
+            </button>
+          </router-link>
         </div>
       </div>
     </div>
+  </div>
   <CommonFooter />
 </template>
 
@@ -28,6 +32,7 @@ import lib from "@/scripts/lib";
 import CommonHeader from "@/components/common/CommonHeader.vue";
 import CommonSideBar from "@/components/common/CommonSideBar.vue";
 import CommonFooter from "@/components/common/CommonFooter.vue";
+import "@/components/common/CommonButtonStyle.css";
 
 export default {
   components: {
@@ -40,23 +45,25 @@ export default {
       items: []
     })
     
-    
-    
     const token = localStorage.getItem("Token"); 
-
-
-    const load = () => {
-      axios.post("/api1/cart/items",{}, {
-        headers: {
-          Authorization: `Bearer ${token}` 
-        }
-      }).then(({data2}) => {
-        console.log(data2);
-        state.items = data2;
-    })
-    };
-    load();
     
+    const loadItems = () => {
+      axios.post("/api1/cart/items", {}, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        // 서버로부터 받은 데이터를 state에 할당합니다.
+        state.items = response.data;
+      })
+      .catch(error => {
+        console.error("Error fetching cart items:", error);
+      });
+    };
+ 
+    
+    loadItems();
     return {token, state, lib}
   }
 }
@@ -96,7 +103,7 @@ export default {
   margin-right: 50px;
 }
 
-.cart .btn {
+.cart .buybtn {
   width:300px;
   display:block;
   margin:0 auto;
