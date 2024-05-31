388d5336eb553f6b978a4135c9fd77b2c1108ba7
kami1152 <kami1152@naver.com>
2024-05-31 14:49:50 +0900

    회원가입 / 로그인 기능 완성

--- src/components/user/register/JoinForm.vue
+++ src/components/user/register/JoinForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="join-container">
+  <form class="join-container" @submit.prevent="register">
     <h2>회원가입</h2>
     <div class="form-group">
       <label for="id">아이디</label>
@@ -101,12 +101,9 @@
     </div>
 
     <br />
-    <button class="register-button" type="button" @click="register">
-      회원가입
-    </button>
-  </div>
+    <button class="register-button" type="submit">회원가입</button>
+  </form>
 </template>
-
 
 <script>
 import axios from "axios";
@@ -129,9 +126,18 @@ export default {
       roadAddress: "",
       jibunAddress: "",
       detailAddress: "",
+      lanlatlocate: {
+        x: "",
+        y: "",
+      },
       idcheck: false,
       resData: null,
     };
+  },
+  computed: {
+    isPasswordMatch() {
+      return this.password === this.passwordConfirm;
+    },
   },
   methods: {
     checkId() {
@@ -143,7 +149,7 @@ export default {
             alert("중복된 아이디 입니다");
             this.id = "";
           } else if (res.data === "donot") {
-            alert("올바르지 않은 아이디 형식 입니다");
+            alert("올바르지 않은 아이디 형식 입니다 (5글자 이상)");
             this.id = "";
           } else {
             alert("사용가능한 아이디입니다");
@@ -154,21 +160,77 @@ export default {
           alert("서버 연결 실패", error);
         });
     },
+
     register() {
-      // 회원가입 로직 추가
-      if (this.password !== this.passwordConfirm) {
-        alert("비밀번호가 일치하지 않습니다.");
+      if (!this.idcheck) {
+        alert("ID 중복 체크를 확인해주세요");
         return;
       }
-      console.log(
-        "회원가입 정보:",
-        this.id,
-        this.name,
-        this.password,
-        this.birth,
-        this.phone
-      );
+      if (!this.isPasswordMatch) {
+        alert("비밀번호가 일치하지 않습니다.");
+        this.password = "";
+        this.passwordConfirm = "";
+        return;
+      }
+
+      alert("회원가입 정보가 유효합니다.");
+
+      const formData = {
+        id: this.id,
+        name: this.name,
+        password: this.password,
+        birth: this.birth,
+        phone: this.phone,
+        nickname: this.nickname,
+        gender: this.gender,
+        postcode: this.postcode,
+        roadAddress: this.roadAddress,
+        jibunAddress: this.jibunAddress,
+        detailAddress: this.detailAddress,
+        locateX: this.lanlatlocate.x,
+        locateY: this.lanlatlocate.y,
+      };
+
+      axios
+        .post("/api2/user/register", formData)
+        .then((response) => {
+          if (response.data.result === "success") {
+            alert("회원가입이 완료되었습니다.");
+            this.$router.push("/Login");
+          } else {
+            alert("회원가입에 실패했습니다.");
+          }
+        })
+        .catch((error) => {
+          alert("서버 연결 실패: " + error.message);
+        });
     },
+
+    getCoordinates(address) {
+      const apiKey = "6c6926ac09f27d3575acd4aee7934548"; // 발급받은 API 키 사용
+      const apiUrl = `https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURIComponent(
+        address
+      )}`;
+      axios
+        .get(apiUrl, {
+          headers: {
+            Authorization: `KakaoAK ${apiKey}`,
+          },
+        })
+        .then((response) => {
+          if (response.data.documents.length > 0) {
+            const { x, y } = response.data.documents[0].address;
+            this.longitude = x;
+            this.latitude = y;
+            this.lanlatlocate.x = this.longitude;
+            this.lanlatlocate.y = this.latitude;
+          }
+        })
+        .catch((error) => {
+          console.error("좌표 검색 실패:", error);
+        });
+    },
+
     openPostcode() {
       new window.daum.Postcode({
         oncomplete: (data) => {
@@ -176,6 +238,11 @@ export default {
           this.postcode = data.zonecode;
           this.roadAddress = data.roadAddress;
           this.jibunAddress = data.jibunAddress;
+          if (this.roadAddress != "") {
+            this.getCoordinates(this.roadAddress);
+          } else {
+            this.getCoordinates(this.jibunAddress);
+          }
         },
         // 팝업 스타일 설정
         width: "100%",
