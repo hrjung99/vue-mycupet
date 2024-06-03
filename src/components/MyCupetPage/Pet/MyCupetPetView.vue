a88d7194323c791edc61e9e07dcd8b9429c4e1f5
sinalol15 <pnsesh01@naver.com>
2024-06-03 11:07:29 +0900

    마이페이지 삭제/수정시 alert창 뜨도록 수정

--- src/components/MyCupetPage/Pet/MyCupetPetView.vue
+++ src/components/MyCupetPage/Pet/MyCupetPetView.vue
@@ -62,9 +62,10 @@ export default {
                 cupet_pet_birth: this.cupet_pet_birth,
                 cupet_pet_type: this.cupet_pet_type
             }).then(response => {
+                alert("수정 되었습니다.");
                 console.log("Pet updated:", response.data);
-                // 필요한 경우 애완동물 목록을 다시 불러오는 등의 작업을 수행할 수 있습니다.
             }).catch(error => {
+                alert("수정에 실패했습니다.");
                 console.error("Error updating pet:", error);
             });
         },
@@ -74,10 +75,12 @@ export default {
             axios
                 .get(`/api1/petDelete?cupet_pet_no=${cupet_pet_no}`) // GET 요청 수정
                 .then(response => {
+                    alert("삭제 되었습니다.");
                     console.log("Pet deleted:", response.data);
                     location.reload();
                 })
                 .catch(error => {
+                    alert("삭제에 실패했습니다.");
                     console.error("Error deleting pet:", error);
                 });
         }
