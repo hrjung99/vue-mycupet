42a28823367fb4818b2c4ef1bd52ae96bddbb9d2
jeongmin <jeongmin0521@naver.com>
2024-06-17 11:20:32 +0900

    주문/결제 내역 시간 로컬 시간으로 수정

--- src/components/shop/PaymentComponent.vue
+++ src/components/shop/PaymentComponent.vue
@@ -5,6 +5,7 @@
     <div class="payPage">
     <div class="col-12">
       <h1>포인트 충전</h1>
+      <br/>
       <label for="cupet_pay_price" class="form-label">충전 금액</label>
       <input type="text" class="form-control" id="cupet_pay_price" v-model="state.form.price"><br/>
       <button class="paybtn" @click="handlePayment()">결제하기</button> &nbsp;
@@ -34,11 +35,20 @@ export default {
   },
   setup() {
     const token = localStorage.getItem("Token");
+
+    // 로컬 시간을 ISO 포맷으로 변환
+    const getLocalISOString = () => {
+      const now = new Date();
+      const timezoneOffset = now.getTimezoneOffset() * 60000;
+      const localISOTime = new Date(now - timezoneOffset).toISOString().slice(0, 19).replace('T', ' ');
+      return localISOTime;
+    };
+
     const state = reactive({
       form: {
         price: "",
         paymentUid: "", 
-        date: new Date().toISOString().slice(0, 16)
+        date: getLocalISOString()
       }
     });
 
@@ -92,6 +102,7 @@ export default {
           if (rsp.success) {
             console.log('Payment succeeded', rsp);
             state.form.paymentUid = rsp.imp_uid; // 결제 UID 저장
+            state.form.date = getLocalISOString(); // 결제 시간 업데이트
             await submit(); // 결제 정보 서버로 전송
           } else {
             console.error('Payment failed', rsp);
@@ -102,10 +113,12 @@ export default {
         console.error('Error during payment process', error);
       }
     };
+
     return { state, submit, handlePayment };
   }
 };
 </script>
+
 
 <style scoped>
 .payPage { 
