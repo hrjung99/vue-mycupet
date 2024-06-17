8f4d850ff6da0401907a71fcd4f5b1aaad3452a1
jeongmin <jeongmin0521@naver.com>
2024-06-17 14:48:33 +0900

    style: 회원조회/관리 페이지

--- src/components/user/AdminUser/UserDetailView.vue
+++ src/components/user/AdminUser/UserDetailView.vue
@@ -1,10 +1,12 @@
 <template>
-    <CommonHeader />
-    <div class="content-container">
-      <CommonSideBar />
-      <div class="table-container"></div>
+  <CommonHeader />
+  <div class="content-container">
+    <CommonSideBar />
+    <div class="UserDetail">
       <div v-if="user">
-        <h2>{{ user.cupet_user_name }}님의 상세 정보</h2>
+        <div class="text-center mb-4">
+          <h2>{{ user.cupet_user_name }}님의 상세 정보</h2>
+        </div>
         <table class="table table-bordered">
           <tbody>
             <tr>
@@ -51,76 +53,99 @@
               <th>포인트</th>
               <td>{{ user.cupet_user_point }}</td>
             </tr>
-            
           </tbody>
         </table>
+        <div class="text-start">
+          <button @click="deleteUser" class="delete-button">회원 삭제</button>
+        </div>
       </div>
-      </div>
-    <CommonFooter />
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  import { ref, onMounted } from 'vue';
-  import { useRoute } from 'vue-router';
-  import CommonHeader from "@/components/common/CommonHeader.vue";
-  import CommonSideBar from "@/components/common/CommonSideBar.vue";
-  import CommonFooter from "@/components/common/CommonFooter.vue";
-  
-  export default {
-    name: "UserDetailView",
-    components: {
-      CommonHeader,
-      CommonSideBar,
-      CommonFooter,
-    },
-    setup() {
-      const route = useRoute();
-      const cupet_user_id = route.params.cupet_user_id;
-      const user = ref(null);
-  
-      onMounted(() => {
-        fetchUserDetails(cupet_user_id);
-      });
-  
-      const fetchUserDetails = (cupet_user_id) => {
-        axios.get(`/api1/usersDetail/${cupet_user_id}`)
-          .then((response) => {
-            user.value = response.data;
+    </div>
+  </div>
+  <CommonFooter />
+</template>
+
+<script>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import CommonHeader from "@/components/common/CommonHeader.vue";
+import CommonSideBar from "@/components/common/CommonSideBar.vue";
+import CommonFooter from "@/components/common/CommonFooter.vue";
+
+export default {
+  name: "UserDetailView",
+  components: {
+    CommonHeader,
+    CommonSideBar,
+    CommonFooter,
+  },
+  setup() {
+    const route = useRoute();
+    const cupet_user_id = route.params.cupet_user_id;
+    const user = ref(null);
+
+    onMounted(() => {
+      fetchUserDetails(cupet_user_id);
+    });
+
+    const fetchUserDetails = (cupet_user_id) => {
+      axios.get(`/api1/usersDetail/${cupet_user_id}`)
+        .then((response) => {
+          user.value = response.data;
+        })
+        .catch((error) => {
+          console.error("Error fetching user details:", error);
+        });
+    };
+
+    const deleteUser = () => {
+      if (confirm("회원을 삭제하시겠습니까?")) {
+        axios.delete('/api1/deleteusers', {
+          data: { cupet_user_id: user.value.cupet_user_id }, 
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(() => {
+            alert("회원이 삭제되었습니다.");
+            window.location.href = '/UserView'; 
           })
           .catch((error) => {
-            console.error("Error fetching user details:", error);
+            console.error("Error deleting user:", error);
+            alert("회원 삭제 중 오류가 발생했습니다.");
           });
-      };
-  
-      return { user, fetchUserDetails };
-    }
-  }
-  </script>
-  
-  <style scoped>
-  .content-container {
-    text-align: center;
-  }
+      }
+    };
 
-  .table-container{
-    margin-left: 50px;
+    return { user, fetchUserDetails, deleteUser };
   }
-  
-  .table {
-    width: 100%;
-    margin-top: 20px;
-    border-collapse: collapse;
-  }
-  
-  .table th, .table td {
-    border: 1px solid #ddd;
-    padding: 8px;
-  }
-  
-  .table th {
-    background-color: #f2f2f2;
-    text-align: left;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
+.content-container {
+  height: 100%;
+  min-height: 80vh;
+  display: flex;
+  flex: 1;
+}
+
+.UserDetail {
+  margin-left: 50px;
+  margin-right: 50px;
+  flex: 0.5;
+}
+
+.delete-button {
+  margin: 20px;
+}
+
+.delete-button:hover {
+    background-color: darkred;
+}
+</style>
--- src/components/user/AdminUser/UserView.vue
+++ src/components/user/AdminUser/UserView.vue
@@ -1,40 +1,42 @@
 <template>
-    <CommonHeader />
-    <div class="content-container">
-        <CommonSideBar />
-        <div class="table-container">
-            <h2>회원 정보</h2>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>선택</th>
-                        <th>이름</th>
-                        <th>닉네임</th>
-                        <th>우편번호</th>
-                        <th>생년월일</th>
-                        <th>전화번호</th>
-                        <th>성별</th>
-                        <th>이메일</th>
-                    </tr>
-                </thead>
+    <div class="main-container">
+        <CommonHeader />
+        <div class="content-container">
+            <CommonSideBar />
+            <div class="UserView">
+                <div class="text-center mb-4"> <h2>회원 정보</h2> </div>
+                <table class="table table-hover">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>선택</th>
+                            <th>이름</th>
+                            <th>닉네임</th>
+                            <th>우편번호</th>
+                            <th>생년월일</th>
+                            <th>전화번호</th>
+                            <th>성별</th>
+                            <th>이메일</th>
+                        </tr>
+                    </thead>
                 <tbody>
                     <tr v-for="(user, idx1) in state.users" :key="idx1" @click="viewDetails(user.cupet_user_id)">
                         <td><input type="checkbox" v-model="state.selectedUsers" :value="user.cupet_user_id" @click.stop></td>
-                        <td>{{ user.cupet_user_name }}</td>
-                        <td>{{ user.cupet_user_nickname }}</td>
-                        <td>{{ user.cupet_user_address }}</td>
-                        <td>{{ user.cupet_user_birth }}</td>
-                        <td>{{ user.cupet_user_phonenumber }}</td>
-                        <td>{{ user.cupet_user_gender }}</td>
-                        <td>{{ user.cupet_user_email }}</td>
-                    </tr>
-                </tbody>
-                <br/>
-                <button @click="deleteSelectedUsers" class="delete-button">선택한 회원 삭제</button>
-            </table>
+                            <td>{{ user.cupet_user_name }}</td>
+                            <td>{{ user.cupet_user_nickname }}</td>
+                            <td>{{ user.cupet_user_address }}</td>
+                            <td>{{ user.cupet_user_birth }}</td>
+                            <td>{{ user.cupet_user_phonenumber }}</td>
+                            <td>{{ user.cupet_user_gender }}</td>
+                            <td>{{ user.cupet_user_email }}</td>
+                        </tr>
+                    </tbody>
+                    <br />
+                    <button @click="deleteSelectedUsers" class="delete-button">회원 삭제</button>
+                </table>
+            </div>
         </div>
+        <CommonFooter />
     </div>
-    <CommonFooter />
 </template>
 
 <script>
@@ -75,7 +77,7 @@ export default {
         };
 
         const viewDetails = (user_id) => {
-            router.push({ name: 'UserDetail', params: { cupet_user_id : user_id } });
+            router.push({ name: 'UserDetail', params: { cupet_user_id: user_id } });
         };
 
         const deleteSelectedUsers = () => {
@@ -115,33 +117,53 @@ export default {
 </script>
 
 <style scoped>
-.content-container {
-    text-align: center;
+html, body {
+  height: 100%;
+  margin: 0;
 }
 
-.table-container {
-    margin-left: 50px;
-    text-align: center;
+.main-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.content-container {
+  display: flex;
+  flex: 1;
+}
+
+.UserView {
+  margin-left: 50px;
+  margin-right: 50px;
+  flex: 1;
+}
+
+.container {
+  padding: 20px;
 }
 
 .table {
-    width: 100%;
-    margin-top: 20px;
-    border-collapse: collapse;
-    text-align: center;
+  margin-top: 30px;
 }
 
-th, td {
-    padding: 10px 10px; /* 셀 안에 여백 */
-    border: 1px solid #ddd; /* 셀 테두리 */
+.table > tbody {
+  border-top: 1px solid #eee;
 }
 
+.table > tbody > tr {
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+.table > tbody > tr:hover {
+  background-color: #f1f1f1;
+}
 
 .delete-button:hover {
     background-color: darkred;
 }
 
-/* 글자와 테두리 사이에 추가적인 여백 */
 td {
     position: relative;
 }
